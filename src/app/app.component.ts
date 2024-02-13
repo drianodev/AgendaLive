681fc2889a6ce4f3a2ccb3d9500660dfc313243d
drianodev <adrianomendes661@gmail.com>
2024-02-13 17:09:58 -0300

    criado services e models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet, 
+    HttpClientModule,
     HomeComponent
   ],
   templateUrl: './app.component.html',
--- src/app/views/home/live-list/live-list.component.ts
+++ src/app/views/home/live-list/live-list.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
+import { LiveService } from '../../../shared/service/live.service';
+import { Live } from '../../../shared/model/live.model';
 
 @Component({
   selector: 'app-live-list',
@@ -12,6 +14,22 @@ import { MatCardModule } from '@angular/material/card';
   templateUrl: './live-list.component.html',
   styleUrl: './live-list.component.css'
 })
-export class LiveListComponent {
+export class LiveListComponent implements OnInit {
 
+  livesPrevious: Live[];
+
+  constructor(
+    public liveService: LiveService
+  ) {}
+
+  ngOnInit(): void {
+      this.getLives();
+  }
+
+  getLives() {
+    this.liveService.getLivesWithFlag('previous').subscribe(data => {
+      this.livesPrevious = data.content;
+      console.log(this.livesPrevious);
+    });
+  }
 }
