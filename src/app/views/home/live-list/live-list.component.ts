56d2279fc65003e332723bec7e7c9b960ae564ee
drianodev <adrianomendes661@gmail.com>
2024-02-13 10:32:34 -0300

    add cards e responsividade

--- src/app/views/home/live-list/live-list.component.ts
+++ src/app/views/home/live-list/live-list.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-live-list',
   standalone: true,
   imports: [
-    MatTabsModule
+    MatTabsModule,
+    MatCardModule
   ],
   templateUrl: './live-list.component.html',
   styleUrl: './live-list.component.css'
